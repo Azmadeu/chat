0c822da40640f0e3b92f7b80730620aff4228686
Azmadeu <doubleworkz@mail.ru>
2018-12-26 08:23:43 +0200

    create redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { addUser } from './actions';
+
 import './index.css';
 
 import chat from './reducers';
 
 const store = createStore(chat);
+
+store.dispatch(addUser('Me'));
 
 ReactDOM.render(
   <Provider store={store}>
