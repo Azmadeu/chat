0f76b9697caaef35d63b84678fb9e69550a6664a
Azmadeu <doubleworkz@mail.ru>
2018-12-27 05:45:54 +0200

    Updates

---
+++ precache-manifest.661088faf58674b4f7e68c66b9fece2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "420fe194f4591c2e84fb",
+    "url": "/chat/static/js/runtime~main.420fe194.js"
+  },
+  {
+    "revision": "57baef72b41bb9fac910",
+    "url": "/chat/static/js/main.57baef72.chunk.js"
+  },
+  {
+    "revision": "8931a7974ac2e79577a5",
+    "url": "/chat/static/js/1.8931a797.chunk.js"
+  },
+  {
+    "revision": "57baef72b41bb9fac910",
+    "url": "/chat/static/css/main.a849a9e5.chunk.css"
+  },
+  {
+    "revision": "9e95ab21bec29c9dcab649f07fd5d820",
+    "url": "/chat/index.html"
+  }
+];
